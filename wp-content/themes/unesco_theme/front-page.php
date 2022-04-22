c971a9829dd96e77e380f94bd8b2619fd7aecd7d
Amalie Nielsen <amal353e@stud.kea.dk>
2022-04-22 14:52:39 +0200

    forside

---
+++ wp-content/themes/unesco_theme/front-page.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Template Name: Front Page Template
+ */
+mesmerize_get_header('homepage');
+?>
+
+    <div id='page-content' class="page-content">
+        <h2>hej</h2>
+        <div class="<?php mesmerize_page_content_wrapper_class(); ?>">
+            <?php
+            while (have_posts()) : the_post();
+                the_content();
+            endwhile;
+            ?>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
