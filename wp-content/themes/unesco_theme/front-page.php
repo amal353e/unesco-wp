09a94f6ff8bf4a7b8f522423f251e51f43e4db28
Amalie Nielsen <amal353e@stud.kea.dk>
2022-04-25 11:16:23 +0200

    flip card bag

--- wp-content/themes/unesco_theme/front-page.php
+++ wp-content/themes/unesco_theme/front-page.php
@@ -6,7 +6,6 @@ mesmerize_get_header('homepage');
 ?>
 
     <div id='page-content' class="page-content">
-        <h2>hej</h2>
         <div class="<?php mesmerize_page_content_wrapper_class(); ?>">
             <?php
             while (have_posts()) : the_post();
@@ -21,8 +20,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/1.png" alt="verdensmål 1">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Afskaf fattigdom</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om afskaffelse af ekstrem fattigdom, at mindske den relative fattigdom og sikre alle adgang til basale ressourcer.</p>
+                        <a href="https://www.verdensmaalene.dk/maal/1">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -33,8 +32,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/2.png" alt="verdensmål 2">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Stop sult</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om at udrydde sult og fejlernæring og skabe bedre forhold for fødevareproduktion.</p>
+                        <a href="https://www.verdensmaalene.dk/maal/2">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -45,8 +44,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/3.png" alt="verdensmål 3">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Sundhed og trivsel</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om at mindske mødredødelighed og dødelighed blandt nyfødte og børn, sætte en stopper for epidemier og øge forebyggelsen af sygdomme samt det generelle sundhedsniveau.</p>
+                    <a href="https://www.verdensmaalene.dk/maal/3">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -58,8 +57,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/4.png" alt="verdensmål 4">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Kvalitetsuddannelse</h1>
-                        <p>kort tekst</p>
+                       <p>Målet handler om at sikre både drenge og piger adgang til gratis grundskole uanset forældrenes indkomst niveau og sikre lige adgangs til højere uddannelse samt information helt generelt.</p>
+                       <a href="https://www.verdensmaalene.dk/maal/4">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -70,8 +69,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/5.png" alt="verdensmål 5">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Ligestilling mellem kønnene</h1>
-                        <p>kort tekst</p>
+                     <p>Målet handler om at styrke kvinders rettigheder og muligheder og stoppe diskrimination af kvinder og piger.</p>
+                     <a href="https://www.verdensmaalene.dk/maal/5">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -82,8 +81,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/6.png" alt="verdensmål 6">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Rent vand og sanitet</h1>
-                        <p>kort tekst</p>
+                       <p>Målet handler om at forbedre vandkvaliteten og sikre en ordentlig forvaltning af vandressourcer, således at der er en universel adgang til rent drikkevand.</p>
+                       <a href="https://www.verdensmaalene.dk/maal/6">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -94,8 +93,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/7.png" alt="verdensmål 7">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Bæredygtig energi</h1>
-                        <p>kort tekst</p>
+                       <p>Målet handler om at sikre en universel adgang til elektricitet til en overkommelig pris og øge andelen af vedvarende energi væsentligt.t</p>
+                       <a href="https://www.verdensmaalene.dk/maal/7">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -106,8 +105,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/8.png" alt="verdensmål 8">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Anstændige jpbs og økonomisk vækst</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om at fremme økonomisk vækst ved at skabe højere produktivitet og teknologiske gennembrud og opnå fuld og produktiv beskæftigelse og anstændige jobs for alle kvinder og mænd i 2030.</p>
+                        <a href="https://www.verdensmaalene.dk/maal/8">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -118,8 +117,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/9.png" alt="verdensmål 9">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Industri innovation og infrastruktur</h1>
-                        <p>kort tekst</p>
+                       <p>Målet handler om at udvikle pålidelig infrastruktur, fremme bæredygtig industri og investere i videnskabelig forskning og innovation.</p>
+                       <a href="https://www.verdensmaalene.dk/maal/9">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -130,8 +129,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/10.png" alt="verdensmål 10">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Mindre ulighed</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om at styrke de laveste indkomstgrupper og fremme en større økonomisk inddragelse af alle samfundsgrupper uanset køn, race og etnicitet.</p>
+                        <a href="https://www.verdensmaalene.dk/maal/10">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -142,8 +141,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/11.png" alt="verdensmål 11">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Bæredygtige byer og lokalsamfund</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om at garantere adgang til sikre og billige boliger samt opgradere beboelse i slumkvarterer og om investeringer i offentlig transport, skabelsen af grønne offentlige rum og forbedring af byplanlægning og bystyring.</p>
+                        <a href="https://www.verdensmaalene.dk/maal/11">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -154,8 +153,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/12.png" alt="verdensmål 12">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Ansvarligt forbrug og produktion</h1>
-                        <p>kort tekst</p>
+                       <p>Målet handler om effektiv styring af vores fælles naturressourcer, og måden vi bortskaffer giftigt affald og forurenende stoffer samt at tilskynde industrier, virksomheder og forbrugere til at genbruge og reducere deres affald.</p>
+                       <a href="https://www.verdensmaalene.dk/maal/12">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -166,8 +165,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/13.png" alt="verdensmål 13">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Klimaindsats</h1>
-                        <p>kort tekst</p>
+                        <p>Målet handler om at begrænse stigningen af den globale gennemsnitstemperatur til to grader over det før-industrielle niveau og styrke modstandsdygtighed og klimatilpasning i sårbare regioner.</p>
+                        <a href="https://www.verdensmaalene.dk/maal/13">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -178,8 +177,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/14.png" alt="verdensmål 14">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Livet i havet</h1>
-                        <p>kort tekst</p>
+                      <p>Målet handler om at skabe en ramme for bæredygtig forvaltning og beskyttelse af havene og kystnære økosystemer og bedre bevarelse og bæredygtig udnyttelse af havbaserede ressourcer.</p>
+                      <a href="https://www.verdensmaalene.dk/maal/14">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -190,8 +189,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/15.png" alt="verdensmål 15">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Livet på land</h1>
-                        <p>kort tekst</p>
+                     <p>Målet handler om at bevare og genoprette brugen af terrestriske økosystemer som skove, vådområder, tørre områder og bjerge inden 2030 og fremme bæredygtig forvaltning af skove og standsning af afskovning.</p>
+                     <a href="https://www.verdensmaalene.dk/maal/15">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -202,8 +201,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/16.png" alt="verdensmål 16">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Fred, retfærdighed og stærke institutioner</h1>
-                        <p>kort tekst</p>
+                     <p>Målet handler om at støtte fredelige og inkluderede samfund, give alle adgang til retssikkerhed og opbygge effektive, ansvarlige og inddragende institutioner på alle niveauer.</p>
+                     <a href="https://www.verdensmaalene.dk/maal/16">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -214,8 +213,8 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/17.png" alt="verdensmål 17">
                     </div>
                     <div class="flip-card-back">
-                        <h1>Partnerskaber for handling</h1>
-                        <p>kort tekst</p>
+                     <p>Målet handler om at styrke nord-syd og syd-syd-samarbejde, fremme international handel og støtte øget eksport til udviklingslandene.</p>
+                     <a href="https://www.verdensmaalene.dk/maal/17">Læs mere</a>
                     </div>
                 </div>
             </div>
@@ -226,23 +225,12 @@ mesmerize_get_header('homepage');
                         <img src="wp-content/themes/unesco_theme/billeder/18.png" alt="verdensmål 18">
                     </div>
                     <div class="flip-card-back">
-                        <h1>De 17 verdensmål</h1>
-                        <p>kort tekst</p>
+                        <p>Læs mere om de 17 verdensmål</p>
+                        <a href="https://www.verdensmaalene.dk/maal">HER</a>
                     </div>
                 </div>
             </div>
 
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img src="wp-content/themes/unesco_theme/billeder/1.png" alt="verdensmål 1">
-                    </div>
-                    <div class="flip-card-back">
-                        <h1>Afskaf fattigdom</h1>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     </div>
