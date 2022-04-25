e51320ea077d231e01238d738b8b92269116a1d0
Amalie Nielsen <amal353e@stud.kea.dk>
2022-04-25 10:39:27 +0200

    flip

--- wp-content/themes/unesco_theme/front-page.php
+++ wp-content/themes/unesco_theme/front-page.php
@@ -14,30 +14,228 @@ mesmerize_get_header('homepage');
             endwhile;
             ?>
             
-            <div class="wrapper_forside">
-                <img src="wp-content/themes/unesco_theme/billeder/1.png" alt="verdensmål 1">
-                <img src="wp-content/themes/unesco_theme/billeder/2.png" alt="verdensmål 2">
-                <img src="wp-content/themes/unesco_theme/billeder/3.png" alt="verdensmål 3">
-                <img src="wp-content/themes/unesco_theme/billeder/4.png" alt="verdensmål 4">
-                <img src="wp-content/themes/unesco_theme/billeder/5.png" alt="verdensmål 5">
-                <img src="wp-content/themes/unesco_theme/billeder/6.png" alt="verdensmål 6">
-                <img src="wp-content/themes/unesco_theme/billeder/7.png" alt="verdensmål 7">
-                <img src="wp-content/themes/unesco_theme/billeder/8.png" alt="verdensmål 8">
-                <img src="wp-content/themes/unesco_theme/billeder/9.png" alt="verdensmål 9">
-                <img src="wp-content/themes/unesco_theme/billeder/10.png" alt="verdensmål 10">
-                <img src="wp-content/themes/unesco_theme/billeder/11.png" alt="verdensmål 11">
-                <img src="wp-content/themes/unesco_theme/billeder/12.png" alt="verdensmål 12">
-                <img src="wp-content/themes/unesco_theme/billeder/13.png" alt="verdensmål 13">
-                <img src="wp-content/themes/unesco_theme/billeder/14.png" alt="verdensmål 14">
-                <img src="wp-content/themes/unesco_theme/billeder/15.png" alt="verdensmål 15">
-                <img src="wp-content/themes/unesco_theme/billeder/16.png" alt="verdensmål 16">
-                <img src="wp-content/themes/unesco_theme/billeder/17.png" alt="verdensmål 17">
-                <img src="wp-content/themes/unesco_theme/billeder/18.png" alt="verdensmål 18">
-            </div>
+     <div class="wrapper_forside">
             <div class="flip-card">
                 <div class="flip-card-inner">
                     <div class="flip-card-front">
-                        <h1>overskrift 1</h1>
+                        <img src="wp-content/themes/unesco_theme/billeder/1.png" alt="verdensmål 1">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Afskaf fattigdom</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/2.png" alt="verdensmål 2">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Stop sult</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/3.png" alt="verdensmål 3">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Sundhed og trivsel</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/4.png" alt="verdensmål 4">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Kvalitetsuddannelse</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/5.png" alt="verdensmål 5">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Ligestilling mellem kønnene</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/6.png" alt="verdensmål 6">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Rent vand og sanitet</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/7.png" alt="verdensmål 7">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Bæredygtig energi</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/8.png" alt="verdensmål 8">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Anstændige jpbs og økonomisk vækst</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/9.png" alt="verdensmål 9">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Industri innovation og infrastruktur</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/10.png" alt="verdensmål 10">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Mindre ulighed</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/11.png" alt="verdensmål 11">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Bæredygtige byer og lokalsamfund</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/12.png" alt="verdensmål 12">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Ansvarligt forbrug og produktion</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/13.png" alt="verdensmål 13">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Klimaindsats</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/14.png" alt="verdensmål 14">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Livet i havet</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/15.png" alt="verdensmål 15">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Livet på land</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/16.png" alt="verdensmål 16">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Fred, retfærdighed og stærke institutioner</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/17.png" alt="verdensmål 17">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>Partnerskaber for handling</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/18.png" alt="verdensmål 18">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>De 17 verdensmål</h1>
+                        <p>kort tekst</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="wp-content/themes/unesco_theme/billeder/1.png" alt="verdensmål 1">
                     </div>
                     <div class="flip-card-back">
                         <h1>Afskaf fattigdom</h1>
@@ -46,6 +244,7 @@ mesmerize_get_header('homepage');
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 <?php get_footer(); ?>
